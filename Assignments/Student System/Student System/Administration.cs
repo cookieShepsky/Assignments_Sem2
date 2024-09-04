5e1b9ef0989244dabeaebe8771d095999d2325d0
Rody Jansen <rody.jansen@student.fontys.nl>
2024-09-04 08:57:57 +0200

    add Administration.cs

---
+++ Assignments/Student System/Student System/Administration.cs
@@ -0,0 +1,12 @@
+﻿namespace Student_System;
+
+internal class Administration
+{
+    private List<> persons = [];
+
+    public Administration(){}
+
+    public void AddPerson(Person p){}
+
+    public void AddStudent()
+}
