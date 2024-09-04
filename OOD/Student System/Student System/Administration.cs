6442059fb45c6eecff149dd84e7ec1a250b1ae86
Rody Jansen <rody.jansen@student.fontys.nl>
2024-09-04 12:27:51 +0200

    chore Administration.cs: finished functionality

--- OOD/Student System/Student System/Administration.cs
+++ OOD/Student System/Student System/Administration.cs
@@ -2,27 +2,41 @@
 
 internal class Administration
 {
-    private List<Person> _persons = [];
-    private List<Student> _students = [];
-    private List<Teacher> _teachers = [];
+    public List<Person> Persons { get; set; } = [];
+    public List<Student> Students { get; set; } = [];
+    public List<Teacher> Teachers { get; set; } = [];
 
     public Administration(){}
 
-    public void AddPerson(Person p){}
+    public void AddPerson(Person p){ Persons.Add(p); }
 
-    public void AddStudent(Student s){}
+    public void AddStudent(Student s){ Students.Add(s); }
 
-    public void AddTeacher(Teacher t){}
+    public void AddTeacher(Teacher t){ Teachers.Add(t); }
 
-    public Person GetPerson(int pcn){}
+    public Person? GetPerson(int pcn)
+    {
+        foreach (Person p in Persons)
+            if (p.Pcn == pcn)
+                return p;
+        return null;
+    }
 
-    public Student GetStudent(int pcn) { }
+    public Student? GetStudent(int pcn)
+    {
+        foreach (Student p in Students)
+            if (p.Pcn == pcn)
+                return p;
+        return null;
+    }
 
-    public Teacher GetTeacher(int pcn) { }
+    public Teacher? GetTeacher(int pcn)
+    {
+        foreach (Teacher p in Teachers)
+            if (p.Pcn == pcn)
+                return p;
+        return null;
+    }
 
-    public List<Person> GetAllPersons() { }
-
-    public List<Student> GetAllStudents() { }
-
-    public List<Teacher> GetAllTeachers() { }
+    // Removed getters, since I use Properties
 }
