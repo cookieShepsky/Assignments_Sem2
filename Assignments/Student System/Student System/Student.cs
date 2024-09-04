d6f4b7ff48f55622e9b1a4862e709379cab4b2e6
Rody Jansen <rody.jansen@student.fontys.nl>
2024-09-04 08:57:38 +0200

    add Student.cs

---
+++ Assignments/Student System/Student System/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace Student_System;
+
+internal class Student : Person
+{
+    private int _nrOfECs;
+
+    public Student(int nrOfECs, string name, int age, int pcn, int yearsAtSchool) : base(name, age, pcn, yearsAtSchool)
+    {
+        _nrOfECs = nrOfECs;
+    }
+
+    public void AddECs(int n) { _nrOfECs += n; }
+}
