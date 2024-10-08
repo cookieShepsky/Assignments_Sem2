7c8d9446b78984a63f38605b524af749eedcf4e1
Rody Jansen <171355049+cookieShepsky@users.noreply.github.com>
2024-10-08 11:16:38 +0200

    chore: add practice project for learning ORM EF core

---
+++ RazorPages practice/Models/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace RazorPages_practice.Models;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
---
+++ RazorPages practice/Pages/People.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPages_practice.Pages
+{
+    public class PeopleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
